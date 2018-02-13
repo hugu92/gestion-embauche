296788607ae42c59e7b3f1c88d1eeb8a3fde8615
Max <github@jacquet.me>
2018-02-13 11:57:47 +0100

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,5 @@
 sfdx force:org:create -f config/project-scratch-def.json -s -d 1
 sfdx force:source:push
-sfdx msm:user:password:set -p ORANGE18 -g User -l User --json
+sfdx msm:user:password:set -p DEMOPL18 -g User -l User --json
 sfdx force:org:display
 sfdx force:org:open
